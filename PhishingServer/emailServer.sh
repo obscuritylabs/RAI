76a72f3cd2bbe1763855dd14a6f894cdd8a60920
Drew Blokzyl <dblokzyl@secureworks.com>
2020-02-26 15:36:52 -0500

    several fixes and best practices

--- PhishingServer/emailServer.sh
+++ PhishingServer/emailServer.sh
@@ -6,17 +6,15 @@
 
 
 # Check for positional argument $1 which should contain the domain
-if [ $1  == "" ]; then 
+if [[ "$1"  == "" ]]; then 
 	echo "Invalid Syntax: Example usage: sudo $0 example.com"
 	exit 1
 fi
 # Check for positional argument $2 which states whether to clean before install 
 cleanup=0
-if [ $2 == "clean" ]; then 
+if [[ "$2" == "clean" ]]; then 
 	cleanup=1
 fi
-# Get private IP for Server
-privIP=$(hostname -i)
 
 # Make all local directories for container volumes
 
@@ -45,9 +43,9 @@ mysqlconf="/app/onlyoffice/mysql/conf.d"
 mysqldata="/app/onlyoffice/mysql/data"
 mysqlinit="/app/onlyoffice/mysql/initdb"
 
-if [ $cleanup == 1 ]; then 
+if [[ $cleanup == 1 ]]; then 
 
-	if [ -d "/app/" ]; then 
+	if [[ -d "/app/" ]]; then 
 		sudo rm -rf "/app/"
 	fi
 fi	
@@ -80,25 +78,25 @@ echo "vm.vfs_cache_pressure=50" | sudo tee -a /etc/sysctl.conf
 ### MySQL config file
 sqlcnf="/app/onlyoffice/mysql/initdb/setup.sql"
 
-sudo echo "CREATE USER 'onlyoffice_user'@'localhost' IDENTIFIED BY 'TempP@ssW0rd12!';" > $sqlcnf
-sudo echo "CREATE USER 'mail_admin'@'localhost' IDENTIFIED BY 'Isadmin123';" >> $sqlcnf
-sudo echo "GRANT ALL PRIVILEGES ON * . * TO 'root'@'%' IDENTIFIED BY 'TempP@ssW0rd12!';" >> $sqlcnf
-sudo echo "GRANT ALL PRIVILEGES ON * . * TO 'onlyoffice_user'@'%' IDENTIFIED BY 'TempP@ssW0rd12!';" >> $sqlcnf
-sudo echo "GRANT ALL PRIVILEGES ON * . * TO 'mail_admin'@'%' IDENTIFIED BY 'Isadmin123';" >> $sqlcnf
-sudo echo "FLUSH PRIVILEGES;" >> $sqlcnf
+echo "CREATE USER 'onlyoffice_user'@'localhost' IDENTIFIED BY 'TempP@ssW0rd12!';" | sudo tee $sqlcnf
+echo "CREATE USER 'mail_admin'@'localhost' IDENTIFIED BY 'Isadmin123';" | sudo tee -a $sqlcnf
+echo "GRANT ALL PRIVILEGES ON * . * TO 'root'@'%' IDENTIFIED BY 'TempP@ssW0rd12!';" | sudo tee -a $sqlcnf
+echo "GRANT ALL PRIVILEGES ON * . * TO 'onlyoffice_user'@'%' IDENTIFIED BY 'TempP@ssW0rd12!';" | sudo tee -a $sqlcnf
+echo "GRANT ALL PRIVILEGES ON * . * TO 'mail_admin'@'%' IDENTIFIED BY 'Isadmin123';" | sudo tee -a $sqlcnf
+echo "FLUSH PRIVILEGES;" >> $sqlcnf
 
 ##### OnlyOffice config file
 oocnf="/app/onlyoffice/mysql/conf.d/onlyoffice.cnf"
 
-sudo echo "[mysqld]" > $oocnf
-sudo echo "sql_mode = 'NO_ENGINE_SUBSTITUTION'" >> $oocnf
-sudo echo "max_connections = 1000" >> $oocnf
-sudo echo "max_allowed_packet = 1048576000" >> $oocnf
+echo "[mysqld]" | sudo tee $oocnf
+echo "sql_mode = 'NO_ENGINE_SUBSTITUTION'" | sudo tee -a $oocnf
+echo "max_connections = 1000" | sudo tee -a $oocnf
+echo "max_allowed_packet = 1048576000" | sudo tee -a $oocnf
 
 sudo chmod -R 755 /app/
 
 # Generate Self Signed Certificate
-cd /app/onlyoffice/CommunityServer/data/certs
+cd /app/onlyoffice/CommunityServer/data/certs || exit 1
 sudo openssl genrsa -out onlyoffice.key 2048
 sudo openssl req -new -key onlyoffice.key -out onlyoffice.csr
 sudo openssl x509 -req -days 365 -in onlyoffice.csr -signkey onlyoffice.key -out onlyoffice.crt
@@ -142,7 +140,7 @@ sudo docker run --net onlyoffice --privileged \
 	-v $msdata:/var/vmail \
 	-v $mscerts:/etc/pki/tls/mailserver \
 	-v $mslogs:/var/log \
-	-h $1 \
+	-h "$1" \
 	onlyoffice/mailserver
 
 # Get the IP of the container running as the mail server so that it can 
@@ -161,7 +159,7 @@ sudo docker run --net onlyoffice -t -t -d --restart=always \
 	-e MYSQL_SERVER_USER=onlyoffice_user \
 	-e MYSQL_SERVER_PASS=TempP@ssW0rd12! \
 	-e DOCUMENT_SERVER_PORT_80_TCP_ADDR=onlyoffice-document-server \
-	-e MAIL_SERVER_API_HOST=$mailsvrIP \
+	-e MAIL_SERVER_API_HOST="$mailsvrIP" \
 	-e MAIL_SERVER_DB_HOST=onlyoffice-mysql-server \
 	-e MAIL_SERVER_DB_NAME=onlyoffice_mailserver \
 	-e MAIL_SERVER_DB_PORT=3306 \
